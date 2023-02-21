ac47162dc6cb76b3222787986f3655204479d78a
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-21 15:45:59 +0200

    add markup galary task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imageContainer = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+imageContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
+console.log(createCardsMarkup(galleryItems))
+
+function createCardsMarkup(galleryItems) {
+    return galleryItems.map(({original, preview, description}) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `
+    }).join('');
+}
+
+// console.log(galleryItems);
